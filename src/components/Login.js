6ba8ec0e0cc15f57190d1cfc185cff02a513fc5a
munyi-victor <munyivictor6@gmail.com>
2024-02-02 23:44:15 +0300

    created login form, modified sign up form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const [passwordType, setPasswordType] = useState("password");
+
+  const showPassword = () => {
+    if (passwordType === "password") {
+      setPasswordType("text");
+    } else {
+      setPasswordType("password");
+    }
+  };
+
+  const handleSubmit = () => {
+    navigate("/");
+  };
+  
   return (
-    <div>
-      Login
+    <div className="form-container">
+      <form className="form-el login-form" onSubmit={handleSubmit}>
+        <h3>Log in</h3>
+        <div>
+          <label for="email">Email:</label>
+          <input type="email" placeholder="Enter email" required />
+        </div>
+        <div>
+          <label for="password">Password:</label>
+          <input type={passwordType} placeholder="Enter Password" required />
+          <span
+            className="show-password show-password-login"
+            onClick={showPassword}
+            style={{ cursor: "pointer" }}
+          >
+            {passwordType === "password" ? "Show" : "Hide"}
+          </span>
+        </div>
+
+        <input type="submit" className="submit-btn" value="Log in" />
+
+        <span>Don't have an account? <a href="/signup">Signup here</a></span>
+      </form>
     </div>
-  )
+  );
 }
 
 export default Login;
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,42 +1,79 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
-  const [passwordVisble, setpasswordVisble] = useState("text")
+  const navigate = useNavigate();
+
+  const [passwordType, setPasswordType] = useState("password");
+
+  const [confirmPasswordType, setConfirmPasswordType2] = useState("password");
 
   const showPassword = () => {
-    if (passwordVisble === "text") {
-      setpasswordVisble("password")
+    if (passwordType === "password") {
+      setPasswordType("text");
     } else {
-      setpasswordVisble("text")
+      setPasswordType("password");
     }
+  }
+
+  const showConfirmPassword = () => {
+    if (confirmPasswordType === "password") {
+      setConfirmPasswordType2("text");
+    } else {
+      setConfirmPasswordType2("password");
+    }
+  };
+
+  const handleSubmit = () => {
+    navigate("/")
   }
 
   return (
     <div className="form-container">
-      <form className="form-el">
-          <h3>Sign Up</h3>
+      <form className="form-el" onSubmit={handleSubmit}>
+        <h3>Sign Up</h3>
         <div>
-          <label for="firstname">First Name</label>
-          <input type="text" placeholder="First Name" />
+          <label for="firstname">First Name:</label>
+          <input type="text" placeholder="First Name" required />
         </div>
         <div>
-          <label for="lastname">Last Name</label>
-          <input type="text" placeholder="Last Name" />
+          <label for="lastname">Last Name:</label>
+          <input type="text" placeholder="Last Name" required />
         </div>
         <div>
-          <label for="email">Email</label>
-          <input type="email" placeholder="Enter email" />
+          <label for="email">Email:</label>
+          <input type="email" placeholder="Enter email" required />
         </div>
         <div>
-          <label for="password">Password</label>
-          <input type={passwordVisble} placeholder="Enter Password"/>
-        </div>
-        <div>
-          <label for="password">Confirm Password</label>
-          <input type="password" placeholder="Confirm Password" />
+          <label for="password">Password:</label>
+          <input type={passwordType} placeholder="Enter Password" required />
+          <span
+            className="show-password"
+            onClick={showPassword}
+            style={{ cursor: "pointer" }}
+          >
+            {passwordType === "password" ? "Show" : "Hide"}
+          </span>
         </div>
 
-        <span className='show' onClick={showPassword} style={{cursor:"pointer"}}>Show password</span>
+        <div>
+          <label for="password">Confirm Password:</label>
+          <input
+            type={confirmPasswordType}
+            placeholder="Confirm Password"
+            required
+          />
+          <span
+            className="show-confirm-password"
+            onClick={showConfirmPassword}
+            style={{ cursor: "pointer" }}
+          >
+            {confirmPasswordType === "password" ? "Show" : "Hide"}
+          </span>
+        </div>
+        <input type="submit" className="submit-btn" value="Sign up" />
+
+        <span>Already have an account? <a href='/login'>Login here</a></span>
       </form>
     </div>
   );
